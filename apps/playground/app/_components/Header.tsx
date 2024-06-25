ad298a747550c2e77f117b9cd7206cef3f92ba67
Not-Sarthak <notsarthakshah@gmail.com>
2024-06-25 15:03:27 +0530

    improvement: add website, docs and playground to monorepo using turborepo

---
+++ apps/playground/app/_components/Header.tsx
@@ -0,0 +1,118 @@
+"use client";
+import Image from "next/image";
+import {
+  brandDarkBorder,
+  brandDarkText,
+} from "../_utils/colors";
+import { Comfortaa } from "next/font/google";
+import { GoArrowUpRight } from "react-icons/go";
+
+const event = ({ action, category, label }:any) => {
+  (window as any).gtag("event", action, {
+    event_category: category,
+    event_label: label,
+  });
+};
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const Header = () => {
+  return (
+    <>
+      <header className="max-w-[1800px] border-b-[1px] border-[#25291C]/40 text-center top-0 flex flex-row items-center lg:justify-between justify-center sm:px-20 px-[12px] py-1.5 w-full">
+        <div className="">
+          <a
+            href="https://www.0rbit.co/"
+            className="flex flex-row gap-3 items-center"
+            onClick={() =>
+              event({ action: "click_logo", category: "Header", label: "Logo" })
+            }
+          >
+            <Image
+              src="/logos/recLight.svg"
+              width={60}
+              height={60}
+              alt="orbit 0rbit logo"
+              className="sm:w-[81px] sm:h-[54px] w-[60px] h-[45px]"
+            />
+            <h1
+              className={`sm:text-[30px] text-[18px] ${brandDarkText} lg:mb-[-15px] mb-[-9px] ${comfortaa.className}`}
+            >
+              playground
+            </h1>
+          </a>
+        </div>
+        <div className=" flex-row gap-3 lg:flex hidden">
+          <a
+            href="http://docs.0rbit.co"
+            className={`rounded-lg px-[24px] py-[3px] ${brandDarkBorder} ${brandDarkText} border-[1px]
+            hover:px-[21px] hover:tracking-widest hover:bg-[#d9decd]`}
+            onClick={() =>
+              event({ action: "click_docs", category: "Header", label: "Docs" })
+            }
+          >
+            Docs
+          </a>
+          <a
+            href="https://mirror.xyz/0x26B11B188E9E69b2426FD6111302E721F423020E"
+            className={`rounded-lg px-[24px] py-[3px] ${brandDarkBorder} ${brandDarkText} border-[1px]
+            hover:px-[21px] hover:tracking-widest hover:bg-[#d9decd]`}
+            onClick={() =>
+              event({
+                action: "click_blogs",
+                category: "Header",
+                label: "Blogs",
+              })
+            }
+          >
+            Blogs
+          </a>
+        </div>
+      </header>
+      <div
+        className={`${brandDarkText} max-w-[1800px] lg:px-20 px-10 text-center  `}
+      >
+        <h2 className="text-[28px] leading-[36px] font-medium lg:block hidden text-[#25291C]">
+          Test <span className="font-['Regular'] text-[#EB8F44]">0rbit</span> in
+          your browser. You can use the code tutorials and test them directly
+          here. For more info, check out our{" "}
+          <span className="inline-flex items-center">
+            <a
+              href="https://docs.0rbit.co/"
+              className="text-[28px] text-[#EB8F44] underline"
+              onClick={() =>
+                event({
+                  action: "click_docs_inline",
+                  category: "Header",
+                  label: "Docs Inline",
+                })
+              }
+            >
+              Docs.
+            </a>
+            <GoArrowUpRight className="text-[#EB8F44]" />
+          </span>
+        </h2>
+        <div className="lg:hidden flex flex-col gap-3 items-center justify-center">
+          <Image
+            src="/mobView.svg"
+            height={150}
+            width={300}
+            alt="mob view"
+            className="mt-[-30px]"
+          />
+          <h2 className=" sm:text-[24px] text-[18px] font-medium sm:leading-[36px] leading-[24px]">
+            {" "}
+            <span className={`text-[#D16D1B]`}>Playground</span> is supported in
+            Desktop view only.
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ apps/playground/app/layout.tsx
@@ -0,0 +1,92 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+// export const metadata: Metadata = {
+//   title: "0rbit | Oracle on Arweave",
+//   description:
+//     "Decentralized Oracle on Arweave ecosystem for accessing off-chain data. Get data from any URL.",
+//     icons: {
+//       icon: [
+//         {
+//           media: "(prefers-color-scheme: light)",
+//           url: "/favicon.ico",
+//           href: "/favicon.ico",
+//         },
+//         {
+//           media: "(prefers-color-scheme: dark)",
+//           url: "/favicon.ico",
+//           href: "/favicon.ico",
+//         },
+//       ],
+//     },
+// };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta property="og:title" content="0rbit | Playground" />
+        <meta
+          property="og:description"
+          content="Decentralized Oracle on Arweave ecosystem for accessing off-chain data. Get data from any URL."
+        />
+        <meta
+          property="og:image"
+          content="https://www.0rbit.co/logos/ogBanner.jpeg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://www.0rbit.co/logos/ogBanner.jpeg"
+        />
+        <meta property="og:url" content="https://www.playground.0rbit.co" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="0rbit | Playground" />
+        <meta
+          name="twitter:description"
+          content="Decentralized Oracle on Arweave ecosystem for accessing off-chain data. Get data from any URL."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.0rbit.co/logos/ogLogo.jpg"
+        />
+
+        <Script
+          strategy="lazyOnload"
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        ></Script>
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.GOOGLE_ANALYTICS}');`}
+        </Script>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <title>0rbit | Playground</title>
+        <meta
+          name="description"
+          content="Decentralized Oracle on Arweave ecosystem for accessing off-chain data. Get data from any URL."
+        />
+      </head>
+      <body className={raleway.className}>{children}</body>
+    </html>
+  );
+}
