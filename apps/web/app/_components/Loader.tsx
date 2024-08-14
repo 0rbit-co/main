ac0958005486f967bb31a5eea546eca6d50bb478
Sarthak <notsarthakshah@gmail.com>
2024-08-15 00:30:06 +0530

    fix: loader

--- apps/web/app/_components/Loader.tsx
+++ apps/web/app/_components/Loader.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen">
-        <Image src={loader} alt="Loading..."  />
+        <Image src={loader} alt="Loading..." width={200} height={200} />
     </div>
   );
 };
