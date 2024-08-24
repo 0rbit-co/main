6b428bea1ab8cde02d64f8cace365a50a43cb3af
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-24 11:08:21 +0530

    team page fixes

--- apps/web/app/team/page.tsx
+++ apps/web/app/team/page.tsx
@@ -28,7 +28,7 @@ const TeamPage = () => {
           0rbit
         </span>
       </h3>
-      <div className="lg:hidden grid grid-cols-2 md::gap-[30px] gap-[15px]">
+      <div className="lg:hidden grid grid-cols-2 md:gap-[30px] gap-[15px] pb-[30px]">
         {" "}
         <TeamCard
           pname="megabyte"
@@ -45,10 +45,8 @@ const TeamPage = () => {
           last={false}
         />
       </div>
-      <div
-        className=" 
-      lg:hidden
-      grid grid-cols-3 md:gap-[30px] gap-[15px]
+      <div className="lg:hidden
+      grid grid-cols-2 md:gap-[30px] gap-[15px]
       items-center justify-center pb-[30px]"
       >
         <TeamCard
@@ -58,31 +56,6 @@ const TeamPage = () => {
           info="Designer"
           last={false}
         />
-        <div className="lg:block hidden place-self-center">
-          <TeamCard
-            pname="megabyte"
-            github="https://github.com/megabyte0x"
-            twitter="https://x.com/megabyte0x?t=WZYKcJAvN-CM7a6yU4lPNQ&s=09"
-            info="Smart Contract Developer"
-            last={false}
-          />
-        </div>
-        {/* <TeamCard
-          pname="lilith"
-          github="https://github.com/0xLPircy"
-          twitter="https://x.com/0xLPircy?t=Ppkfa4HmoEsfPMEeYJCisw&s=09"
-          info="Frontend Developer"
-          last={false}
-        /> */}
-        <div className="lg:block hidden place-self-center">
-          <TeamCard
-            pname="lucifer"
-            github="https://github.com/Lucifer0x17"
-            twitter="https://x.com/Lucifer0x17?t=fH5LRms3xy2hSPLJbNubaA&s=09"
-            info="Backend and Infrastructure"
-            last={false}
-          />
-        </div>
         <TeamCard
           pname="sarthak"
           github="https://github.com/Not-Sarthak"
@@ -112,13 +85,6 @@ const TeamPage = () => {
           info="Smart Contract Developer"
           last={false}
         />
-        {/* <TeamCard
-          pname="lilith"
-          github="https://github.com/0xLPircy"
-          twitter="https://x.com/0xLPircy?t=Ppkfa4HmoEsfPMEeYJCisw&s=09"
-          info="Frontend Developer"
-          last={false}
-        /> */}
         <TeamCard
           pname="lucifer"
           github="https://github.com/Lucifer0x17"
@@ -134,7 +100,6 @@ const TeamPage = () => {
           last={true}
         />
       </div>
-      {/* <Footer /> */}
     </main>
     </>
   );
