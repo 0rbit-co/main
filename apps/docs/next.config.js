f0fd0baf8571daa71b7d476a7616d7fa83015a3a
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-01-17 17:07:01 +0530

    fix: static export

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -4,4 +4,11 @@ const withNextra = require('nextra')({
   defaultShowCopyCode: true
 })
 
-module.exports = withNextra
+module.exports = {
+  ...withNextra,
+  assetPrefix: "./",
+  output: 'export',  // Enables static HTML export
+  images: {
+    unoptimized: true // Required for static export
+  }
+}
